24e6eeb77ed904aa6a4ded14e95f52d7a07502f6
bouscs <busanix@gmail.com>
2022-03-15 12:07:47 -0300

    add wireframe activities

---
+++ app/src/main/java/com/example/davidzonefiscal/ResultadoConsultaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.davidzonefiscal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoConsultaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_consulta)
+    }
+}
