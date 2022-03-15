adb3fb8a88749475cd4f8de480fd5bb360ae8afc
Tobias Dornelles <tobiasdornelles@hotmail.com>
2022-03-15 13:37:29 -0300

    Minor initial changes

--- app/src/main/java/com/example/davidzonefiscal/ConsultaPlacaActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/ConsultaPlacaActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ConsultaPlacaActivity : AppCompatActivity() {
+
+    private lateinit var
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consulta_placa)
