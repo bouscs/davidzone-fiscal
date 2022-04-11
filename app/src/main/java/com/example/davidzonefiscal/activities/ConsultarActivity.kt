243fe27ed1c0976b2d22c6b69d35e66a8a9111ee
David Megumi Shinjo <dvdshinjo@gmail.com>
2022-04-11 15:27:39 -0300

    consultar placa backend/internet errors

--- app/src/main/java/com/example/davidzonefiscal/activities/ConsultarActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/activities/ConsultarActivity.kt
@@ -11,6 +11,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.davidzonefiscal.R
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
@@ -34,18 +35,38 @@ class ConsultarActivity : AppCompatActivity() {
 
     }
 
-    private fun consulta(){
-        if(etPlaca.text.isNullOrEmpty()||etPlaca.text.length!=7){
+    private fun consulta() {
+        var internetError = false
+        var backendError = false
+
+        if (internetError) {
+            Snackbar.make(etPlaca, "Sem conexão de internet", Snackbar.LENGTH_LONG)
+                .setAction("Tente novamente") {
+                    consulta()
+                }
+                .show()
+        } else if (backendError){
+            Snackbar.make(etPlaca, "Erro no servidor. Se o problema persistir ligue 0800-000-0000", Snackbar.LENGTH_LONG)
+                .setAction("Tente novamente") {
+                    consulta()
+                }
+                .show()
+
+        // TODO validar placa mais formal (3 char seguido de 4 int)
+        } else if(etPlaca.text.isNullOrEmpty()||etPlaca.text.length!=7){
             textField.error = "Digite uma placa válida!"
         } else if(etPlaca.text.toString()!="AAA1111"&&etPlaca.text.toString()!="AAA1112"){
             textField.error = "Veículo não encontrado na Base de Dados."
         } else if(etPlaca.text.toString()=="AAA1111"){
             val intentConsult = Intent(this@ConsultarActivity, ResultadoConsultaActivity::class.java )
+            // Ticket valido
             startActivity(intentConsult)
         } else if(etPlaca.text.toString()=="AAA1112"){
+            // Ticket não valido
             val intentConsultNoTicket = Intent(this@ConsultarActivity, ResultadoNoTicketActivity::class.java )
             startActivity(intentConsultNoTicket)
         }
+
 
     }
 
