b2fa6a173cb0a11d3313ca1dbc48eb49620c451f
john richard <joao.ensenat@gmail.com>
2022-04-20 21:35:33 -0300

    changes on cameraX

--- app/src/main/java/com/example/davidzonefiscal/activities/CameraPreviewActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/activities/CameraPreviewActivity.kt
@@ -3,6 +3,7 @@ package com.example.davidzonefiscal.activities
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.media.Image
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -21,6 +22,8 @@ import com.example.davidzonefiscal.databinding.ActivityCameraPreviewBinding
 import com.google.common.util.concurrent.ListenableFuture
 import java.io.File
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -39,6 +42,9 @@ class CameraPreviewActivity : AppCompatActivity() {
     // Executor de thread separada
     private lateinit var imgCaptureExecutor: ExecutorService
 
+    // Output directory
+    private lateinit var outputDirectory: File
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -48,12 +54,18 @@ class CameraPreviewActivity : AppCompatActivity() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
         imgCaptureExecutor = Executors.newSingleThreadExecutor()
+        outputDirectory = getOutputDirectory()
 
         // chamar o startCamera()
         startCamera()
 
         binding.abTirarFoto.setOnClickListener{
-            takePhoto()
+            //func criada pelo professor em video
+            //takephoto()
+
+            //func criada com ajuda do video de um indiano
+            takePhoto2()
+
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 blinkPreview()
             }
@@ -100,6 +112,47 @@ class CameraPreviewActivity : AppCompatActivity() {
                 })
 
         }
+    }
+
+    private fun takePhoto2(){
+        val imageCapture = imageCapture ?: return
+        val fileNameFormat = "yy-MM-dd-HH-mm-ss-SSS"
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(fileNameFormat,
+                Locale.getDefault())
+                .format(System
+                    .currentTimeMillis()) + ".jpg")
+
+        val outputFileOption = ImageCapture
+                .OutputFileOptions
+                .Builder(photoFile)
+                .build()
+        imageCapture.takePicture(
+            outputFileOption, ContextCompat.getMainExecutor(this),
+            object: ImageCapture.OnImageSavedCallback{
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(photoFile)
+                    Toast.makeText(binding.root.context, "A imagem foi salva no diretório: $savedUri", Toast.LENGTH_SHORT).show()
+                    Log.i("CameraPreview", "A imagem foi salva no diretório: $savedUri")
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    Toast.makeText(binding.root.context, "Erro ao salvar foto.", Toast.LENGTH_SHORT).show()
+                    Log.e("CameraPreview", "Exceção ao gravar arquivo da foto: $exception")
+                }
+            }
+        )
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
--- app/src/main/java/com/example/davidzonefiscal/activities/TirarFotosActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/activities/TirarFotosActivity.kt
@@ -1,16 +1,34 @@
 package com.example.davidzonefiscal.activities
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.davidzonefiscal.R
 import com.example.davidzonefiscal.databinding.ActivityTirarFotosBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TirarFotosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTirarFotosBinding
+
+    private lateinit var photoFile: File
+    lateinit var currentPhotoPath: String
+    private val PICTURE_FROM_CAMERA: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +36,50 @@ class TirarFotosActivity : AppCompatActivity() {
         binding = ActivityTirarFotosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStart.setOnClickListener{
+        binding.btnStart.setOnClickListener {
             cameraProviderResult.launch(android.Manifest.permission.CAMERA)
         }
+        /*
+        binding.btnStart.setOnClickListener {
+            takePicture()
+        } */
     }
+
+    /*
+    private fun takePicture(){
+        val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        photoFile = createImageFile()
+        val uri = FileProvider.getUriForFile(this, "com.example.davidzonefiscal.fileprovider", photoFile)
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        startActivityForResult(pictureIntent, PICTURE_FROM_CAMERA)
+    }
+
+    private fun createImageFile(): File {
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_",
+            ".jpg",
+            storageDir
+        ).apply {
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == PICTURE_FROM_CAMERA) {
+                val uri = FileProvider.getUriForFile(
+                    this,
+                    "com.example.davidzonefiscal.fileprovider",
+                    photoFile
+                )
+                binding.imgView1!!.setImageURI(uri)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    } */
 
     private fun abrirPreview() {
         val intentStart = Intent(this@TirarFotosActivity, CameraPreviewActivity::class.java)
@@ -36,4 +94,6 @@ class TirarFotosActivity : AppCompatActivity() {
                 Toast.makeText(this, "Sem permissões para o uso da câmera.", Toast.LENGTH_SHORT).show()
             }
         }
-}
+    }
+
+     
