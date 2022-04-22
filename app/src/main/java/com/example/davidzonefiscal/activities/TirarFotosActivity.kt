3ac7f80951c0c2433b5ecfd12223bf45705f0d30
Joao Ricardo <joao.ensenat@gmail.com>
2022-04-22 11:17:43 -0300

    TirarFotosActivity complete

--- app/src/main/java/com/example/davidzonefiscal/activities/TirarFotosActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/activities/TirarFotosActivity.kt
@@ -25,10 +25,10 @@ import java.util.*
 class TirarFotosActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTirarFotosBinding
-
+    private var i = 0
     private lateinit var photoFile: File
     lateinit var currentPhotoPath: String
-    private val PICTURE_FROM_CAMERA: Int = 1
+    private val pictureFromCamera: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,22 +36,23 @@ class TirarFotosActivity : AppCompatActivity() {
         binding = ActivityTirarFotosBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStart.setOnClickListener {
-            cameraProviderResult.launch(android.Manifest.permission.CAMERA)
-        }
         /*
         binding.btnStart.setOnClickListener {
+            cameraProviderResult.launch(android.Manifest.permission.CAMERA)
+        }*/
+
+        binding.btnStart.setOnClickListener {
             takePicture()
-        } */
+        }
     }
 
-    /*
+
     private fun takePicture(){
         val pictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         photoFile = createImageFile()
         val uri = FileProvider.getUriForFile(this, "com.example.davidzonefiscal.fileprovider", photoFile)
         pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-        startActivityForResult(pictureIntent, PICTURE_FROM_CAMERA)
+        startActivityForResult(pictureIntent, pictureFromCamera)
     }
 
     private fun createImageFile(): File {
@@ -68,18 +69,38 @@ class TirarFotosActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == PICTURE_FROM_CAMERA) {
+            if (requestCode == pictureFromCamera) {
                 val uri = FileProvider.getUriForFile(
                     this,
                     "com.example.davidzonefiscal.fileprovider",
                     photoFile
                 )
-                binding.imgView1!!.setImageURI(uri)
+                i++
+                when (i) {
+                    1 -> {
+                        binding.imgView1!!.setImageURI(uri)
+                        binding.tvCount.text = "1/4"
+                        binding.btnStart.text = "Próximo"
+                    }
+                    2 -> {
+                        binding.imgView2!!.setImageURI(uri)
+                        binding.tvCount.text = "2/4"
+                    }
+                    3 -> {
+                        binding.imgView3!!.setImageURI(uri)
+                        binding.tvCount.text = "3/4"
+                    }
+                    4 -> {
+                        binding.imgView4!!.setImageURI(uri)
+                        binding.tvCount.text = "4/4"
+                        binding.btnStart.text = "Enviar"
+                    }
+                }
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
-    } */
+    }
 
     private fun abrirPreview() {
         val intentStart = Intent(this@TirarFotosActivity, CameraPreviewActivity::class.java)
