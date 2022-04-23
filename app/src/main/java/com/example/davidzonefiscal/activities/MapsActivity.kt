75d83091f5d81d05c7a135a25b39c53c3e1dc1e4
Miguel Teixeira <81261687+CookieNinjaBR@users.noreply.github.com>
2022-04-23 12:07:06 -0300

    Current Location Button
    Added a current location button to the map

--- app/src/main/java/com/example/davidzonefiscal/activities/MapsActivity.kt
+++ app/src/main/java/com/example/davidzonefiscal/activities/MapsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.davidzonefiscal.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.davidzonefiscal.R
 import com.example.davidzonefiscal.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,7 +28,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
+
+    private val LOCATION_PERMISSION = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +44,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Botão regsitrar irregularidade diretamente.
         binding.btnregistradireto.setOnClickListener{
@@ -51,6 +65,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    private fun getLocAccess() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mMap.isMyLocationEnabled = true
+        }
+        else
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                mMap.isMyLocationEnabled = true
+            }
+            else {
+                Toast.makeText(this, "Usuario não permitiu o uso do GPS", Toast.LENGTH_LONG).show()
+                finish()
+            }
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -62,9 +97,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        getLocAccess()
+        //val puc = LatLng(-22.834065, -47.052522)
+        //mMap.addMarker(MarkerOptions().position(puc).title("Marker in PUC"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(puc, 15f))
 
-        val puc = LatLng(-22.834065, -47.052522)
-        mMap.addMarker(MarkerOptions().position(puc).title("Marker in PUC"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(puc, 15f))
     }
+
+
+
 }
