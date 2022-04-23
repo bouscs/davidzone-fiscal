2e06e56c6c819fc3b2bcd96a86f45160e8d88b65
Miguel Teixeira <81261687+CookieNinjaBR@users.noreply.github.com>
2022-04-23 20:23:08 -0300

    Finished visual part of itinerary
    Changed itinerarios class.
    Added full marker implementation and changed app design accordingly.

--- app/src/main/java/com/example/davidzonefiscal/entities/Itinerario.kt
+++ app/src/main/java/com/example/davidzonefiscal/entities/Itinerario.kt
@@ -2,16 +2,8 @@ package com.example.davidzonefiscal.entities
 
 import com.google.android.gms.maps.model.LatLng
 
-data class Itinerario(    val rua1: String,  // rua do ponto
-                          val ponto1_1: LatLng,
-                          val ponto1_2: LatLng,
-                          val ponto1_3: LatLng,
-                          val rua2: String , // rua do ponto
-                          val ponto2_1: LatLng,
-                          val ponto2_2: LatLng,
-                          val ponto2_3: LatLng,
-                          val rua3: String,  // rua do ponto
-                          val ponto3_1: LatLng,
-                          val ponto3_2: LatLng,
-                          val ponto3_3: LatLng) {}
+data class Itinerario(    val rua: String,//rua dos pontos
+                          val ponto: LatLng,
+                          val ponto2: LatLng,
+                          val ponto3: LatLng) {}
 
